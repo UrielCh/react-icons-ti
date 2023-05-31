4eafcf3bedfed053fd8eeb96fea3528c983bdf47
urielch <uriel.chemouni@gmail.com>
2023-05-31 18:09:36 +0300

    fix dual @

--- mod.ts
+++ mod.ts
@@ -17,8 +17,8 @@
  *   "imports": {
  *     "preact":  "https://esm.sh/preact@10.15.1",
  *     "preact/": "https://esm.sh/preact@10.15.1/",
- *     "react-icons/ti":  "https://cdn.jsdelivr.net/gh/urielch/react-icons-ti@1.0.4/mod.ts",
- *     "react-icons/ti/": "https://cdn.jsdelivr.net/gh/urielch/react-icons-ti@1.0.4/ico/",
+ *     "react-icons/ti":  "https://cdn.jsdelivr.net/gh/urielch/react-icons-ti@1.0.5/mod.ts",
+ *     "react-icons/ti/": "https://cdn.jsdelivr.net/gh/urielch/react-icons-ti@1.0.5/ico/",
  *   }
  * }
  * ```
@@ -26,7 +26,7 @@
  * ## Direct import sample
  * 
  * ```ts
- * import { TiAdjustBrightness } from "https://deno.land/x/react_icons_ti@1.0.4/mod.ts"
+ * import { TiAdjustBrightness } from "https://deno.land/x/react_icons_ti@1.0.5/mod.ts"
  * ```
  * 
  * ## import_map import sample

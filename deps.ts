0592375674747ff12e9308ef39e1d25dbb49f4ed
urielch <uriel.chemouni@gmail.com>
2023-05-31 18:34:42 +0300

    bump version

--- deps.ts
+++ deps.ts
@@ -1,2 +1,2 @@
-// export { GenIcon, type IconBaseProps } from "https://deno.land/x/react_icons@1.0.5/mod.ts";
-export { GenIcon, type IconBaseProps } from "https://cdn.jsdelivr.net/gh/urielch/react-icons@1.0.5/mod.ts";
+// export { GenIcon, type IconBaseProps } from "https://deno.land/x/react_icons@1.0.6/mod.ts";
+export { GenIcon, type IconBaseProps } from "https://cdn.jsdelivr.net/gh/urielch/react-icons@1.0.6/mod.ts";

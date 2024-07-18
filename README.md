1ad25a68dbed68b81d303915f6e7f0d0ebfd371f
urielch <uriel.chemouni@gmail.com>
2024-07-18 10:53:43 +0200

    update doc

--- README.md
+++ README.md
@@ -8,7 +8,7 @@
 
 [See available icons here](https://react-icons.deno.dev/ti)
 
-## install packahe
+## install the module
 
 ```bash
 deno add @preact-icons/ti
@@ -17,36 +17,15 @@ pnpm dlx jsr add @preact-icons/ti
 bunx jsr add @preact-icons/ti
 ```
 
-You may need to update your preact mapping to:
- - "preact": "npm:preact@10.22.1",
- - "preact/jsx-runtime": "npm:preact@10.22.1/jsx-runtime",
- - "preact/hooks": "npm:preact@10.22.1/hooks",
-
-
-# Typicons icons for preact
-
-[![JSR](https://jsr.io/badges/@preact-icons/ti)](https://jsr.io/@preact-icons/ti)
-
-**License** [CC BY-SA 3.0](https://creativecommons.org/licenses/by-sa/3.0/)
-
-**Project** [http://s-ings.com/typicons/](http://s-ings.com/typicons/)
-
-[See available icons here](https://react-icons.deno.dev/ti)
-
-## install packahe
-
-```bash
-deno add @preact-icons/ti
-dnpx jsr add @preact-icons/ti
-pnpm dlx jsr add @preact-icons/ti
-bunx jsr add @preact-icons/ti
+You may need to update your preact mapping to avoid mixing JSR and http import:
+Currently Deno fresh import preact using https://esm.sh/preact http import can not be mixed with JSR package, so you may need to update your preact mapping:
+```json
+{
+ "preact": "npm:preact@10.22.1",
+ "preact/jsx-runtime": "npm:preact@10.22.1/jsx-runtime",
+ "preact/hooks": "npm:preact@10.22.1/hooks",
+}
 ```
-
-You may need to update your preact mapping to:
- - "preact": "npm:preact@10.22.1",
- - "preact/jsx-runtime": "npm:preact@10.22.1/jsx-runtime",
- - "preact/hooks": "npm:preact@10.22.1/hooks",
-
 
 ## import an icon from all icons
 
@@ -65,4 +44,3 @@ or using default export
 ```ts
 import TiAdjustBrightness from "react-icons/ti/TiAdjustBrightness.ts"
 ```
-
--- mod.ts
+++ mod.ts
@@ -10,7 +10,7 @@
  * 
  * [See available icons here](https://react-icons.deno.dev/ti)
  * 
- * ## install packahe
+ * ## install the module
  * 
  * ```bash
  * deno add @preact-icons/ti
@@ -19,36 +19,15 @@
  * bunx jsr add @preact-icons/ti
  * ```
  * 
- * You may need to update your preact mapping to:
- *  - "preact": "npm:preact@10.22.1",
- *  - "preact/jsx-runtime": "npm:preact@10.22.1/jsx-runtime",
- *  - "preact/hooks": "npm:preact@10.22.1/hooks",
- * 
- * 
- * # Typicons icons for preact
- * 
- * [![JSR](https://jsr.io/badges/@preact-icons/ti)](https://jsr.io/@preact-icons/ti)
- * 
- * **License** [CC BY-SA 3.0](https://creativecommons.org/licenses/by-sa/3.0/)
- * 
- * **Project** [http://s-ings.com/typicons/](http://s-ings.com/typicons/)
- * 
- * [See available icons here](https://react-icons.deno.dev/ti)
- * 
- * ## install packahe
- * 
- * ```bash
- * deno add @preact-icons/ti
- * dnpx jsr add @preact-icons/ti
- * pnpm dlx jsr add @preact-icons/ti
- * bunx jsr add @preact-icons/ti
+ * You may need to update your preact mapping to avoid mixing JSR and http import:
+ * Currently Deno fresh import preact using https://esm.sh/preact http import can not be mixed with JSR package, so you may need to update your preact mapping:
+ * ```json
+ * {
+ *  "preact": "npm:preact@10.22.1",
+ *  "preact/jsx-runtime": "npm:preact@10.22.1/jsx-runtime",
+ *  "preact/hooks": "npm:preact@10.22.1/hooks",
+ * }
  * ```
- * 
- * You may need to update your preact mapping to:
- *  - "preact": "npm:preact@10.22.1",
- *  - "preact/jsx-runtime": "npm:preact@10.22.1/jsx-runtime",
- *  - "preact/hooks": "npm:preact@10.22.1/hooks",
- * 
  * 
  * ## import an icon from all icons
  * 
@@ -67,8 +46,8 @@
  * ```ts
  * import TiAdjustBrightness from "react-icons/ti/TiAdjustBrightness.ts"
  * ```
- * 
  * @module
+ * 
  */
 
 import { GenIcon } from "./deps.ts";
